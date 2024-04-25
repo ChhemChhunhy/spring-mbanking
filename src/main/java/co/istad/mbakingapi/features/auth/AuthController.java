8a7e5b3492f37cfd74ba0c16c49270795921eb71
chhunhychhunhy121828@gmail.com <chhunhychhunhy121828@gmail.com>
2024-04-25 15:20:34 +0700

    change password

--- src/main/java/co/istad/mbakingapi/features/auth/AuthController.java
+++ src/main/java/co/istad/mbakingapi/features/auth/AuthController.java
@@ -1,20 +1,25 @@
 package co.istad.mbakingapi.features.auth;
 
 import co.istad.mbakingapi.features.auth.dto.AuthResponse;
+import co.istad.mbakingapi.features.auth.dto.ChangePasswordRequest;
 import co.istad.mbakingapi.features.auth.dto.LoginRequest;
 import co.istad.mbakingapi.features.auth.dto.RefreshTokenRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
+    @PutMapping("/change-password")
+    void changePassword(@Valid @RequestBody ChangePasswordRequest changePasswordRequest ,@AuthenticationPrincipal Jwt jwt){
+        authService.chnagePasswords(jwt,changePasswordRequest);
+    }
+
     @PostMapping("/login")
     AuthResponse login(@Valid @RequestBody LoginRequest loginRequest){
         return authService.login(loginRequest);
--- src/main/java/co/istad/mbakingapi/features/auth/AuthServiceImpl.java
+++ src/main/java/co/istad/mbakingapi/features/auth/AuthServiceImpl.java
@@ -1,18 +1,23 @@
 package co.istad.mbakingapi.features.auth;
 
+import co.istad.mbakingapi.domain.User;
 import co.istad.mbakingapi.features.auth.dto.AuthResponse;
+import co.istad.mbakingapi.features.auth.dto.ChangePasswordRequest;
 import co.istad.mbakingapi.features.auth.dto.LoginRequest;
 import co.istad.mbakingapi.features.auth.dto.RefreshTokenRequest;
 import co.istad.mbakingapi.features.token.TokenService;
+import co.istad.mbakingapi.features.user.UserRepository;
 import co.istad.mbakingapi.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
@@ -21,6 +26,7 @@ import org.springframework.security.oauth2.server.resource.authentication.Bearer
 import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthenticationToken;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
@@ -35,6 +41,8 @@ public class AuthServiceImpl implements AuthService{
     private final DaoAuthenticationProvider daoAuthenticationProvider;
     private final JwtAuthenticationProvider jwtAuthenticationProvider;
     private final TokenService tokenService;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public AuthResponse refresh(RefreshTokenRequest refreshTokenRequest) {
@@ -59,5 +67,34 @@ public class AuthServiceImpl implements AuthService{
 
         return tokenService.createToken(auth);
 
+    }
+
+    @Override
+    public void chnagePasswords(Jwt jwt, ChangePasswordRequest changePasswordRequest) {
+        User user = userRepository.findByPhoneNumber(jwt.getId()).orElseThrow(
+                ()-> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "User not found"
+                )
+        );
+        if (!changePasswordRequest.password().equals(changePasswordRequest.confirmPassword())){
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST,
+                    "Password does not match"
+            );
+        }
+        if (!passwordEncoder.matches(changePasswordRequest.oldPassword(),user.getPassword())){
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST,
+                    "Password does not match"
+            );
+        }
+        if (passwordEncoder.matches(changePasswordRequest.password(),user.getPassword())){
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST,
+                    "New password cannot be the same as the old password"
+            );
+        }
+        user.setPassword(passwordEncoder.encode(changePasswordRequest.password()));
     }
 }
